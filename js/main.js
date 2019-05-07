cf5c3b777765687aee551e8ab53f8ea9f7c35811
石河凌平 <r.ishikawa1996@gmail.com>
2019-05-07 13:50:09 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,90 @@
+(() => {
+  'use strict';
+
+  const firebaseConfig = {
+      // メモ帳にあるお
+    };
+    firebase.initializeApp(firebaseConfig);
+
+  const db = firebase.firestore();
+  const collection = db.collection('messages');
+
+  const auth = firebase.auth();
+  let me = null;
+
+  const message = document.getElementById('message');
+  const form = document.querySelector('form');
+  const messages = document.getElementById('messages');
+  const login = document.getElementById('login');
+  const logout = document.getElementById('logout');
+
+
+
+
+  login.addEventListener('click', () => {
+    auth.signInAnonymously();
+  });
+
+  logout.addEventListener('click', () => {
+    auth.signOut();
+  });
+
+  auth.onAuthStateChanged(user =>{
+    if (user) {
+      me = user;
+
+      while (messages.firstChild) {
+        messages.removeChild(messages.firstChild);
+      }
+
+      collection.orderBy('created').onSnapshot(snapshot => {
+    snapshot.docChanges().forEach(change => {
+      if (change.type === 'added') {
+        const li = document.createElement('li');
+        const d = change.doc.data();
+        li.textContent = d.uid.substr(0, 8) + ': ' + d.message;
+        messages.appendChild(li);
+      }
+    });
+  }, error => {});
+      console.log(`ログインしているのは: ${user.uid}`);
+      login.classList.add('hidden');
+      [logout, form, messages].forEach(el => {
+        el.classList.remove('hidden');
+      });
+      message.focus();
+      return;
+    }
+    me = null;
+    console.log('誰もログインしてない');
+    login.classList.remove('hidden');
+      [logout, form, messages].forEach(el => {
+        el.classList.add('hidden');
+      });
+  });
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const val = message.value.trim();
+    if (val === "") {
+      return;
+    }
+
+    message.value = '';
+    message.focus();
+
+    collection.add({
+      message: val,
+      created: firebase.firestore.FieldValue.serverTimestamp(),
+      uid: me ? me.uid : 'nobody'
+    })
+    .then(doc => {
+      console.log(`${doc.id} added!`);
+    })
+    .catch(error => {
+      console.log('document add error!');
+      console.log(error);
+    });
+  });
+})();
